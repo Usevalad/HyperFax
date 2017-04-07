5aea5cd11e2330e3e34b909a1ba97800bae12c70
Usevalad <usevalad.uladzimiravich@gmail.com>
2017-04-07 16:43:41 +0300

    arrows fixed

--- HyperFax/app/src/main/java/com/vsevolod/swipe/addphoto/holder/IconTreeItemHolder.java
+++ HyperFax/app/src/main/java/com/vsevolod/swipe/addphoto/holder/IconTreeItemHolder.java
@@ -3,17 +3,19 @@ package com.vsevolod.swipe.addphoto.holder;
 /**
  * Created by vsevolod on 06.04.17.
  */
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import com.github.johnkil.print.PrintView;
+
 import com.vsevolod.flowstreelibrary.model.TreeNode;
 import com.vsevolod.swipe.addphoto.R;
 
 public class IconTreeItemHolder extends TreeNode.BaseNodeViewHolder<IconTreeItemHolder.IconTreeItem> {
     private TextView tvValue;
-    private PrintView arrowView;
+    private ImageView arrowView;
 
     public IconTreeItemHolder(Context context) {
         super(context);
@@ -25,21 +27,22 @@ public class IconTreeItemHolder extends TreeNode.BaseNodeViewHolder<IconTreeItem
         final View view = inflater.inflate(R.layout.layout_icon_node, null, false);
         tvValue = (TextView) view.findViewById(R.id.node_value);
         tvValue.setText(value.text);
-        arrowView = (PrintView) view.findViewById(R.id.arrow_icon);
+        arrowView = (ImageView) view.findViewById(R.id.arrow_icon);
 
         return view;
     }
 
     @Override
     public void toggle(boolean active) {
-        arrowView.setIconText(context.getResources()
-                .getString(active ? R.string.ic_keyboard_arrow_down : R.string.ic_keyboard_arrow_right));
+        arrowView.setImageResource(active ? R.drawable.ic_arrow_down : R.drawable.ic_arrow_right);
     }
 
     public static class IconTreeItem {
+        public int icon;
         public String text;
 
-        public IconTreeItem(String text) {
+        public IconTreeItem(int icon, String text) {
+            this.icon = icon;
             this.text = text;
         }
     }
