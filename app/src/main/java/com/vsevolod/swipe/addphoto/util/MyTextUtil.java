e5c5f7359623b23b7481becf278b771ddef4b5f9
Usevalad <usevalad.uladzimiravich@gmail.com>
2017-07-18 13:24:05 +0300

    highlight fixed

--- app/src/main/java/com/vsevolod/swipe/addphoto/util/MyTextUtil.java
+++ app/src/main/java/com/vsevolod/swipe/addphoto/util/MyTextUtil.java
@@ -5,19 +5,14 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
-
-import com.vsevolod.swipe.addphoto.R;
-import com.vsevolod.swipe.addphoto.config.MyApplication;
-
-import static com.vsevolod.swipe.addphoto.R.color.highlight;
 
 /**
  * Created by vsevolod on 7/17/17.
  */
 
 public class MyTextUtil {
+
     public static SpannableStringBuilder toBold(String boldText) {
         SpannableStringBuilder sb = new SpannableStringBuilder(boldText);
         StyleSpan b = new StyleSpan(android.graphics.Typeface.BOLD);
@@ -25,28 +20,30 @@ public class MyTextUtil {
         return sb;
     }
 
-//    private static SpannableStringBuilder toHighlight(String highlight) {
-//        SpannableStringBuilder highlighted = new SpannableStringBuilder(highlight);
-//        highlighted.setSpan(new BackgroundColorSpan(R.color.cardview_dark_background),
-//                0, highlight.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        return highlighted;
-//    }
-
     public static SpannableStringBuilder highLightMatches(String text, String searchString) {
         SpannableStringBuilder highlighted = new SpannableStringBuilder(text);
         if (!TextUtils.isEmpty(searchString)) {
-            String lowerCaseText = text.toLowerCase();
-            String lowerCaseSearchString = searchString.toLowerCase();
-            int searchStringLength = searchString.length();
-
-            if (lowerCaseText.contains(lowerCaseSearchString)) {
-                int start = lowerCaseText.indexOf(lowerCaseSearchString);
-                int end = start + searchStringLength;
-                highlighted.setSpan(new BackgroundColorSpan(Color.YELLOW),
-                        start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
+            recursion(highlighted, text.toLowerCase(), searchString.toLowerCase());
         }
         return highlighted;
     }
 
+    private static void recursion(SpannableStringBuilder highlighted, String text, String searchString) {
+        int searchStringLength = searchString.length();
+        if (text.contains(searchString)) {
+            setSpan(highlighted, text.indexOf(searchString), searchStringLength, text.length());
+            if (text.length() >= searchString.length()) {
+                String subString = text.substring(text.indexOf(searchString) + searchStringLength, text.length());
+                if (subString.contains(searchString)) {
+                    recursion(highlighted, subString, searchString);
+                }
+            }
+        }
+    }
+
+    private static void setSpan(SpannableStringBuilder highlighted, int start, int searchStringLength, int textLength) {
+        start = highlighted.length() > textLength ? start + highlighted.length() - textLength : start;
+        int end = start + searchStringLength;
+        highlighted.setSpan(new BackgroundColorSpan(Color.YELLOW), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
 }
