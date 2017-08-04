b096d8be75ea530bc951f0f269a86b2f3e20e48b
Usevalad <usevalad.uladzimiravich@gmail.com>
2017-08-04 22:47:09 +0300

    !

--- app/src/main/java/com/vsevolod/swipe/addphoto/util/PathConverter.java
+++ app/src/main/java/com/vsevolod/swipe/addphoto/util/PathConverter.java
@@ -6,9 +6,12 @@ import android.net.Uri;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.firebase.crash.FirebaseCrash;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by vsevolod on 21.04.17.
@@ -27,7 +30,6 @@ public final class PathConverter {
 
     public String getFullPath(Uri uri, int photoResource) {
         Log.e(TAG, "getFullPath");
-        String result;
         return getFullPathElse(uri);
     }
 
@@ -47,12 +49,8 @@ public final class PathConverter {
                         ? cursor.getString(columnIndex)
                         : cursor.getString(cursor.getColumnIndex(projection[0]));
             }
-            if (result == null) {
-                FirebaseCrash.log(TAG + " result = " + result);
-            }
         } catch (NullPointerException e) {
             e.printStackTrace();
-            FirebaseCrash.log(TAG + " " + e.getMessage());
         } finally {
             if (cursor != null) {
                 cursor.close();
@@ -60,7 +58,7 @@ public final class PathConverter {
         }
         mContext = null;
 
-        if (result == null) {
+        if (TextUtils.isEmpty(result)) {
             result = getFulPathLollipop(uri);
         }
 
@@ -91,76 +89,4 @@ public final class PathConverter {
         Log.e(TAG, "getFulPathLollipop: path " + result);
         return result;
     }
-//    public String getFullPath(Uri uri, int photoResource) {
-//        Log.e(TAG, "getFullPath");
-//        String result;
-//        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP ||
-//                Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP_MR1 &&
-//                        photoResource == Constants.SELECT_PICTURE_REQUEST) {
-//            return getFulPathLollipop(uri);
-//        } else {
-//            return getFullPathElse(uri);
-//        }
-//    }
-//
-//    private String getFullPathElse(@NonNull Uri uri) {
-//        Log.e(TAG, "getFullPathElse");
-//        Cursor cursor = null;
-//        String result = null;
-//        try {
-//            String[] projection = {MediaStore.Images.Media.DATA};
-//            cursor = mContext.getContentResolver().query(uri, projection, null, null, null);
-//            if (cursor == null) {
-//                result = uri.getPath();
-//            } else {
-//                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//                cursor.moveToFirst();
-//                result = cursor.getString(columnIndex) != null
-//                        ? cursor.getString(columnIndex)
-//                        : cursor.getString(cursor.getColumnIndex(projection[0]));
-//            }
-//            if (result == null) {
-//                FirebaseCrash.log(TAG + " result = " + result);
-//            }
-//        } catch (NullPointerException e) {
-//            e.printStackTrace();
-//            FirebaseCrash.log(TAG + " " + e.getMessage());
-//        } finally {
-//            if (cursor != null) {
-//                cursor.close();
-//            }
-//        }
-//        mContext = null;
-//
-//        if (result == null) {
-//            result = uri.getPath();
-//        }
-//
-//        Log.e(TAG, "getFullPath: " + result);
-//        return result;
-//    }
-//
-//    private String getFulPathLollipop(Uri uri) {
-//        Log.e(TAG, "getFulPathLollipop");
-//        String wholeID = DocumentsContract.getDocumentId(uri);
-//        // Split at colon, use second item in the array
-//        String id = wholeID.split(":")[1];
-//        String[] column = {MediaStore.Images.Media.DATA};
-//        // where id is equal to
-//        String sel = MediaStore.Images.Media._ID + "=?";
-//        String result = null;
-//        Cursor cursor = mContext.getContentResolver().
-//                query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-//                        column, sel, new String[]{id}, null);
-//
-//        if (cursor != null) {
-//            int columnIndex = cursor.getColumnIndex(column[0]);
-//            if (cursor.moveToFirst()) {
-//                result = cursor.getString(columnIndex);
-//            }
-//            cursor.close();
-//        }
-//        Log.e(TAG, "getFulPathLollipop: path " + result);
-//        return result;
-//    }
 }
